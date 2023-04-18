b8a7470ab7416305d69840b4f9e9d7a32f900fbd
Никита Вологин <Nikita.Vologin@mail.ru>
2023-04-18 21:36:36 +0500

    Старый проект был разделён на новые классы в соответствии с паттерном MVC. Старый код переписан в соответствии с новыми классами. Классы написаны в отдельных файлах.

---
+++ Sokoban/GameClasses/Box.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sokoban
+{
+    public class Box
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public Image Model { get; }
+        public Box(int x, int y)
+        {
+            X = x;
+            Y = y;
+            Model = new Bitmap(Path.Combine(
+                new DirectoryInfo(Directory.GetCurrentDirectory()).Parent.Parent.FullName.ToString(), "Models\\Box.png"));
+        }
+    }
+}
---
+++ Sokoban/GameClasses/Player.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sokoban
+{
+    public class Player
+    {
+        public Player(int x, int y)
+        {
+            X = x; Y = y;
+            Model = new Bitmap(Path.Combine(new DirectoryInfo(Directory.GetCurrentDirectory()).Parent.Parent.FullName.ToString(), "Models\\MainHero.png")); 
+        }
+        public Image Model { get; }
+        public float X { get; set; }
+        public float Y { get; set; }
+        public int DirX { get; set; }
+        public int DirY { get; set; }
+    }
+}
