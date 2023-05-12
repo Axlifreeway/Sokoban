6e18a13760c39d7827877474f837541c3fb33c8e
Александро Алешкин <Axlifreeway@hotmail.com>
2023-05-12 14:52:37 +0500

    Changelog: * Мелкие изменения в системе победы - теперь победа зависит не от заданного вручную числа, а от переменной Levels.LevelsCount * Теперь для понятности динамическая матрица карты была изменена на с числового массива на массив клеток. Требуется рефакторинг касаемо задания двух одинаковых массивов Cells и Dynamicmap юез создания ссылки со второго на первый

--- Sokoban/GameClasses/Levels.cs
+++ Sokoban/GameClasses/Levels.cs
@@ -8,6 +8,7 @@ namespace Sokoban
 {
     public static class Levels
     {
+        public static int LevelsCount = 4;
         public static int currentLevel = 1;
         public static int Size = 128;
 
