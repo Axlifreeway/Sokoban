203718c0e22e21fc2820938d0d50ad577d1ac0f2
Александро Алешкин <Axlifreeway@hotmail.com>
2023-05-12 14:18:58 +0500

    Changelog: * Изменил ширину(Levels.Width) и длину(Levels.Height) клетки на общий параметр -> Levels.Size. * Провёл рефакторинг класса PlayerServis, убрал ненужное повторение кода для X и для Y.

--- Sokoban/GameClasses/Servis/PlayerServis.cs
+++ Sokoban/GameClasses/Servis/PlayerServis.cs
@@ -9,66 +9,43 @@ namespace Sokoban.GameClasses.Servis
 {
     internal class PlayerServis
     {
-        public static bool Move(int x, int y, Map map, Player player)
+        public static bool Move(Map map, Player player)
         {
-            int moveX = x * player.DirX;
-            int moveY = y * player.DirY;
-            var px = player.X;
-            var py = player.Y;
+            int moveX = Levels.Size * player.DirX;
+            int moveY = Levels.Size * player.DirY;
             if (player.X + moveX > 0 && player.X + moveX < map.Width
-                && map[player.DirX + player.X / Levels.Width, player.Y / Levels.Height] != 4)
+                && player.Y + moveY > 0 && player.Y + moveY < map.Height
+                && map[player.DirX + player.X / Levels.Size, player.DirY + player.Y / Levels.Size] != 4)
             {
                 foreach (var box in map.Boxes)
                 {
-                    if (player.X + moveX == box.X && player.Y  == box.Y
-                        && (map[player.DirX + box.X / Levels.Width, box.Y / Levels.Height] == 4
-                        || map[player.DirX + box.X / Levels.Width, box.Y / Levels.Height] == 2))
+                    if (player.X + moveX == box.X && player.Y + moveY == box.Y
+                        && (map[player.DirX + box.X / Levels.Size, player.DirY + box.Y / Levels.Size] == 4
+                        || map[player.DirX + box.X / Levels.Size, player.DirY + box.Y / Levels.Size] == 2))
                         return false;
                 }
-                map[player.X / Levels.Width, player.Y / Levels.Height] = 0;
+                map[player.X / Levels.Size, player.Y / Levels.Size] = 0;
                 player.X += moveX;
-                map[player.X / Levels.Width, player.Y / Levels.Height] = 3;
+                player.Y += moveY;
+                map[player.X / Levels.Size, player.Y / Levels.Size] = 3;
             }
-
-            if (player.Y + moveY > 0 && player.Y + moveY < map.Height
-                && map[player.X / Levels.Width, player.Y / Levels.Height + player.DirY] != 4)
-            {
-                foreach (var box in map.Boxes)
-                {
-                    if (player.X == box.X && player.Y + moveY == box.Y 
-                    && (map[box.X / Levels.Width, box.Y / Levels.Height + player.DirY] == 4
-                    || map[box.X / Levels.Width, box.Y / Levels.Height + player.DirY] == 2))
-                        return false;
-                }
-                map[player.X / Levels.Width, player.Y / Levels.Height] = 0;
-                player.Y += y * player.DirY;
-                map[player.X / Levels.Width, player.Y / Levels.Height] = 3;
-            }
-
             return false;
         }
 
         public static void BoxMove(Map map, Box box)
         {
-            if (map.Player.DirX * Levels.Width + box.X < map.Width && map.Player.DirX * Levels.Width + box.X > 0
-                && map[map.Player.DirX + box.X / Levels.Width, box.Y / Levels.Height] != 4
-                && map[map.Player.DirX + box.X / Levels.Width, box.Y / Levels.Height] != 2)
+            int moveX = Levels.Size * map.Player.DirX;
+            int moveY = Levels.Size * map.Player.DirY;
+            if (moveX + box.X < map.Width && moveX + box.X > 0
+                && moveY + box.Y < map.Height && moveY + box.Y > 0
+                && map[map.Player.DirX + box.X / Levels.Size, map.Player.DirY + box.Y / Levels.Size] != 4
+                && map[map.Player.DirX + box.X / Levels.Size, map.Player.DirY + box.Y / Levels.Size] != 2)
             {
-                map[box.X / Levels.Width, box.Y / Levels.Height] = 3;
-                box.X += map.Player.DirX * Levels.Width;
-                map[box.X / Levels.Width, box.Y / Levels.Height] = 2;
+                map[box.X / Levels.Size, box.Y / Levels.Size] = 3;
+                box.X += moveX;
+                box.Y += moveY;
+                map[box.X / Levels.Size, box.Y / Levels.Size] = 2;
             }
-
-
-            if (map.Player.DirY * Levels.Height + box.Y < map.Height && map.Player.DirY * Levels.Height + box.Y > 0
-                && map[box.X / Levels.Width, box.Y / Levels.Height + map.Player.DirY] != 4
-                && map[box.X / Levels.Width, box.Y / Levels.Height + map.Player.DirY] != 2)
-            {
-                map[box.X / Levels.Width, box.Y / Levels.Height] = 3;
-                box.Y += map.Player.DirY * Levels.Height;
-                map[box.X / Levels.Width, box.Y / Levels.Height] = 2;
-            }
-
             map.CheckOnWin();
         }
     }
