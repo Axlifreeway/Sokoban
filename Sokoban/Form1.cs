9c2119935e510f68007baa577b88b1f09250ac30
Никита Вологин <Nikita.Vologin@mail.ru>
2023-05-13 12:01:19 +0500

    Рефакторинг

--- Sokoban/Form1.cs
+++ Sokoban/Form1.cs
@@ -23,9 +23,6 @@ namespace Sokoban
         public static Map map;
         public int TickCount = 0;
 
-        public int TickCount = 0;
-
-
         public GameForm()
         {
             InitializeComponent();
